e5377b3458466b886aef4ee51382f03b62fe7505
Ylli Ramadani <119905715+yesm4n@users.noreply.github.com>
2023-06-05 08:16:38 +0200

    Update app.js
    Drag event added

--- app.js
+++ app.js
@@ -61,6 +61,52 @@ function createNewElement() {
   const listItem = document.createElement("div");
   listItem.classList.add("list-item");
   counter++;
+  if (counter >= 1) {
+    listItem.classList.add("draggable");
+    listItem.setAttribute("draggable", true);
+    listItem.style.cursor = "move";
+    listItem.addEventListener("dragstart", () => {
+      listItem.classList.add("dragging");
+    });
+
+    listItem.addEventListener("dragend", () => {
+      listItem.classList.remove("dragging");
+    });
+  }
+
+  const containerDragOver = document.querySelector(".d-container");
+  containerDragOver.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(containerDragOver, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement === null) {
+      containerDragOver.appendChild(draggable);
+    } else {
+      containerDragOver.insertBefore(draggable, afterElement);
+    }
+  });
+
+  function getDragAfterElement(containerDragOver, y) {
+    const draggableElements = [
+      ...containerDragOver.querySelectorAll(".draggable:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        console.log(offset);
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      {
+        offset: Number.NEGATIVE_INFINITY,
+      }
+    ).element;
+  }
 
   const iconCheck = document.createElement("img");
   iconCheck.classList.add("icon-check");
